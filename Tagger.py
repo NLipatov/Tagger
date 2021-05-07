9b6bf5bf9a665dda4a8fbf39d449df7723deed9e
NLipatov <80638093+NLipatov@users.noreply.github.com>
2021-05-07 14:09:23 +0300

    Update Tagger.py

--- Tagger.py
+++ Tagger.py
@@ -1,5 +1,6 @@
 import xlrd
 import xlwt
+import pymorphy2
 def Path_definer():
     global path
     GotAnswer = False
@@ -11,134 +12,209 @@ def Path_definer():
             break
         if IQ.upper() == "N":
             GotAnswer = True
-            path = r'%s' % (input("Вставьте путь до файла:"))
+            path = r'%s' % (input("Укажите полный путь к файлу:"))
             break
         else:
-            print("Пожалуйста, ответьте утвердительно или отрицательно используя буквы 'Y' 'N'!")
+            print("Пожалуйста, ответьте утвердительно или отрицательно используя буквы \"Y\" или \"N\"!")
 Path_definer()
-workbook = xlrd.open_workbook(path)
+morph = pymorphy2.MorphAnalyzer()
+workbook = xlrd.open_workbook( path )
 workbookw = xlwt.Workbook()
-sheet = workbook.sheet_by_index(0)
-sheet1 = workbookw.add_sheet("Sheet1", cell_overwrite_ok=True)
-dictionary = {"Сумка": ["bag", "handbag", "pouche", "shoulder bag", "backpack"],"Одежда": ["blouse","hoodie","jacket", "coat", "tshirt", "t-shirt", "parka", "dress", "anorak", "blazer", "costume"],"Обувь":["sandals", "sneakers"],"Аксессуары": ["earrings",] }
-BagValues = ["bag", "handbag", "pouche", "shoulder bag", "backpack"]
-ClothingValues = ["blouse","hoodie","jacket", "coat", "tshirt", "t-shirt", "parka", "dress", "anorak", "blazer", "costume"]
-ShoesValues = ["sandals", "sneakers"]
-AccesoriesValues = ["earrings"]
+sheet = workbook.sheet_by_index( 0 )
+sheet1 = workbookw.add_sheet( "Sheet1", cell_overwrite_ok=True )
+# смотреть комментарий по позициям из двух слов в словаре на линии 86!
+dictionary = {"Сумка": ["bag", "bags", "handbag", "pouche", "shoulder bag", "backpack"],
+              "Одежда": ["shorts", "coat", "leggings", "scarf", "gloves", "bodysuit", "skirt", "swimsuit", "pants",
+                         "pant", "sweater", "cardigan", "cardigans", "sweatshirt", "shirt", "bikini", "bikinis",
+                         "trousers", "blouse", "hoodie", "jacket", "coat", "tshirt", "t-shirt", "t-shirts", "t-shirts", "parka", "dress",
+                         "anorak", "blazer", "blazers", "costume"],
+              "Обувь": ["sandals", "sneakers", "boots", "low-top", "pumps", "loafers", "mules", "trainers", "footwear"],
+              "Бижутерия": ["earrings", "necklace" ],
+              "Очки": ["sunglasses", "glasses", ]}
+Sumka = morph.parse( 'сумка' )[0]
+Odezhda = morph.parse( 'одежда' )[0]
+Obuv = morph.parse( 'обувь' )[0]
+Aksessuary = morph.parse( 'бижутерия' )[0]
+Ochki = morph.parse( 'очки ' )[0]
 rows = sheet.nrows
 rowsInWork = 1
-print("Описаний грузов в файле:" + str(rows - 1))
+print( "Описаний грузов в файле:" + str( rows - 1 ) )
 FailureAtTagAssignmentProcessCounter = 0
 FailureAtTagAssignmentProcessList = []
-
-
 while (rowsInWork - 1) != (rows - 1):
-	dictionaryValues = []
-	description = sheet.cell_value((rowsInWork), 2).replace(",", "").lower().split()
-	descriptionLength = len(description)
-	if descriptionLength == 0:
-		print("Нет описания!" + str(rowsInWork))
-	else:
-		print("Рассматриваю груз.")	
-	counter = 0
-	tagCounter = 0 #Необходим для написания в ячейку "НЕ ОБНАРУЖЕНО КЛЮЧЕЙ!". see line 80
-	KeyCounter = 0
-	CounterOfIteratedKeys = 0
-	def KeyCountDefiner(dictionary):
-		return len(dictionary.keys())
-	KeyCounter = KeyCountDefiner(dictionary)
-	watchlist = []
-	
+    dictionaryValues = []
+    description = sheet.cell_value( (rowsInWork), 2 ).replace( ",", " " ).lower().split()
+    print( description )
+    descriptionLength = len( description )
+    if descriptionLength == 0:
+        print( "Нет описания!" + str( rowsInWork ) )
+    else:
+        print( "Рассматриваю груз." )
+    counter = 0
+    tagCounter = 0  # Необходим для написания в ячейку "НЕ ОБНАРУЖЕНО КЛЮЧЕЙ!". see line 80
+    KeyCounter = 0
+    CounterOfIteratedKeys = 0
 
 
-	#Назначаем списку watchlist списки-значения словаря и выводим все списки значений словаря следующей компандой:
-	while True:
-		a = (list(dictionary.values())[(CounterOfIteratedKeys)])
-		watchlist.append(a)
-		CounterOfIteratedKeys += 1
-		if KeyCounter == CounterOfIteratedKeys:
-			break		
-	#print(range(0, (CounterOfIteratedKeys - 1)))	OUTPUT: range(0, 1)	
-	#rint(watchlist)	OUTPUT: [['bag', 'handbag', 'pouche', 'shoulder bag'], ['coat', 'tshirt', 't-shirt', 'parka', 'dress', 'anorak', 'blazer', 'costume']]
-	#Смотрю, есть ли в списках из значений каждого ключа слово из описания(дескрипшн)
+    def KeyCountDefiner(dictionary):
+        return len( dictionary.keys() )
 
-	#print(dictionary["Сумка"]) OUTPUT ['bag', 'handbag', 'pouche', 'shoulder bag']
 
-	for i in BagValues:
-		matching = [s for s in description if i in s]
-		matchingLen = len(matching)
-		if matchingLen != 0:
-			print("Обнаружена сумка")
-			sheet1.write(rowsInWork, 6, "сумка")
-			tagCounter += 1
-			break
-			#Строчка на запись в соответствующую ячейку, что это одежда
-	for i in ClothingValues:
-		matching = [s for s in description if i in s]
-		matchingLen = len(matching)
-		if matchingLen != 0:
-			print("Обнаружена одежда")
-			sheet1.write(rowsInWork, 7, "одежда")
-			tagCounter += 1
-			break
-			#Строчка на запись в соответствующую ячейку, что это одежда
-	for i in ShoesValues:
-		matching = [s for s in description if i in s]
-		matchingLen = len(matching)
-		if matchingLen != 0:
-			print("Обнаружена обувь")
-			sheet1.write(rowsInWork, 8, "обувь")
-			tagCounter += 1
-			break
-			#Строчка на запись в соответствующую ячейку, что это одежда
-	for i in AccesoriesValues:
-		matching = [s for s in description if i in s]
-		matchingLen = len(matching)
-		if matchingLen != 0:
-			print("Обнаружены аксессуары")
-			sheet1.write(rowsInWork, 9, "аксессуары")
-			tagCounter += 1
-			break
-			#Строчка на запись в соответствующую ячейку, что это одежда						
-	if tagCounter == 0:
-		sheet1.write(rowsInWork, 10, "НЕ ОБНАРУЖЕНО КЛЮЧЕЙ!")	
-		FailureAtTagAssignmentProcessCounter += 1
-		FailureAtTagAssignmentProcessList.append((rowsInWork))
-	counter = ((rows - 1) - rowsInWork)
-	rowsInWork += 1	
+    KeyCounter = KeyCountDefiner( dictionary )
+    watchlist = []
 
-	print("Остаётся грузов: " + str(counter))	
-	print("***************************************************************************************")
-	tagCounter = 0
-	if rowsInWork == (rows):
-		print(("Готово!").center(60, "="))
-		if FailureAtTagAssignmentProcessCounter == 0:
-			print("Всем грузам присвоены соответствующие тэги.".center(60))
-		if FailureAtTagAssignmentProcessCounter != 0:
-			print("\n\n\n\n\n\nВнимание: найдены ошибки!")
-			print("Необработанных описаний: " + str(FailureAtTagAssignmentProcessCounter) + " шт." + "Необходимо обработать вручную!")
-			print("Грузы без тэга: ", end="")
-			print(*FailureAtTagAssignmentProcessList, sep=", ")
-	
+    # Назначаем списку watchlist списки-значения словаря и выводим все списки значений словаря следующей компандой:
+    while True:
+        a = (list( dictionary.values() )[(CounterOfIteratedKeys)])
+        watchlist.append( a )
+        CounterOfIteratedKeys += 1
+        if KeyCounter == CounterOfIteratedKeys:
+            break
+    # print(range(0, (CounterOfIteratedKeys - 1)))	OUTPUT: range(0, 1)
+    # rint(watchlist)	OUTPUT: [['bag', 'handbag', 'pouche', 'shoulder bag'], ['coat', 'tshirt', 't-shirt', 'parka', 'dress', 'anorak', 'blazer', 'costume']]
+    # Смотрю, есть ли в списках из значений каждого ключа слово из описания(дескрипшн)
 
-nrows = sheet.nrows #Соответствует заполнению по оси Y
-ncols = sheet.ncols #Соответствует заполнению по оси X
+    # print(dictionary["Сумка"]) OUTPUT ['bag', 'handbag', 'pouche', 'shoulder bag']
+    Finded_categories = []
+    Bag_counter = 0
+    Clothes_counter = 0
+    Shoes_counter = 0
+    Accesories_counter = 0
+    Glasses_counter = 0
+
+    for i in description:
+        if i in (dictionary["Сумка"]):
+            print( "Совпадение. Это — сумка! \n(ключевое слово: " + str( i ) + ")" )
+            # Строчка на запись в соответствующую ячейку, что это сумка
+            Bag_counter += 1
+            tagCounter += 1
+    for i in description:
+        if i in (dictionary["Одежда"]):
+            print( "Совпадение. Это — одежда! \n(ключевое слово: " + str( i ) + ")" )
+            Clothes_counter += 1
+            tagCounter += 1
+    for i in description:
+        if i in (dictionary["Обувь"]):
+            print( "Совпадение. Это — обувь! \n(ключевое слово: " + str( i ) + ")" )
+            Shoes_counter += 1
+            tagCounter += 1
+    for i in description:
+        if i in (dictionary["Бижутерия"]):
+            print( "Совпадение. Это — бижутерия! —  \n(ключевое слово: " + str( i ) + ")" )
+            Accesories_counter += 1
+            tagCounter += 1
+    for i in description:
+        if i in (dictionary["Очки"]):
+            print( "Совпадение. Это — очки! —  \n(ключевое слово: " + str( i ) + ")" )
+            Glasses_counter += 1
+            tagCounter += 1
+    # Состоящим из двух слов ключам необходимо прописывать исключения, как это сделано для "flip flop". "flip flop" записывается в description как 2 слова - flips и flops
+    if "flip" and "flops" in description:
+        print( "Совпадение. Это — обувь! \n(ключевое слово: " + str( "flips flops" ) + ")" )
+        Shoes_counter += 1
+        tagCounter += 1
+    if "flipflops" in description:
+        print( "Совпадение. Это — обувь! \n(ключевое слово: " + str( "flips flops" ) + ")" )
+        Shoes_counter += 1
+        tagCounter += 1
+    if "flip-flops" in description:
+        print( "Совпадение. Это — обувь! \n(ключевое слово: " + str( "flips flops" ) + ")" )
+        Shoes_counter += 1
+        tagCounter += 1
+    bag = str( Sumka.make_agree_with_number( Bag_counter ).word )
+    clothes = str( Odezhda.make_agree_with_number( Clothes_counter ).word )
+    shoes = str( Obuv.make_agree_with_number( Shoes_counter ).word )
+    accesories = str( Aksessuary.make_agree_with_number( Accesories_counter ).word )
+    glasses = str( Ochki.make_agree_with_number( Glasses_counter ).word )
+
+    # Нужно написать логику определения проставления запятой после написания числа совпадений и наименования
+    if Bag_counter == 1 and (tagCounter - Bag_counter) == 0:
+        Finded_categories.append( str( bag ) )
+    elif Bag_counter == 1 and (tagCounter - Bag_counter) != 0:
+        Finded_categories.append( str( bag ) + "," )
+    elif Bag_counter > 1 and (tagCounter - Bag_counter) == 0:
+        Finded_categories.append( str( Bag_counter ) + " " + str( bag ) )
+    elif Bag_counter > 1 and (tagCounter - Bag_counter) != 0:
+        Finded_categories.append( str( Bag_counter ) + " " + str( bag ) + "," )
+    if Clothes_counter == 1 and (tagCounter - Bag_counter - Clothes_counter) == 0:
+        Finded_categories.append( str( clothes ) )
+    elif Clothes_counter == 1 and (tagCounter - Bag_counter - Clothes_counter) != 0:
+        Finded_categories.append( str( clothes ) + "," )
+    elif Clothes_counter > 1 and (tagCounter - Bag_counter - Clothes_counter) == 0:
+        Finded_categories.append( str( Clothes_counter ) + " " + str( clothes ) )
+    elif Clothes_counter > 1 and (tagCounter - Bag_counter - Clothes_counter) != 0:
+        Finded_categories.append( str( Clothes_counter ) + " " + str( clothes ) + "," )
+    if Shoes_counter == 1 and (tagCounter - Bag_counter - Clothes_counter - Shoes_counter) == 0:
+        Finded_categories.append( str( shoes ) )
+    elif Shoes_counter == 1 and (tagCounter - Bag_counter - Clothes_counter - Shoes_counter) != 0:
+        Finded_categories.append( str( shoes ) + "," )
+    elif Shoes_counter > 1 and (tagCounter - Bag_counter - Clothes_counter - Shoes_counter) == 0:
+        Finded_categories.append( str( Shoes_counter ) + " " + str( shoes ) )
+    elif Shoes_counter > 1 and (tagCounter - Bag_counter - Clothes_counter - Shoes_counter) != 0:
+        Finded_categories.append( str( Shoes_counter ) + " " + str( shoes ) + "," )
+    if Accesories_counter == 1 and (
+            tagCounter - Bag_counter - Clothes_counter - Shoes_counter - Accesories_counter) == 0:
+        Finded_categories.append( str( accesories ) )
+    elif Accesories_counter == 1 and (
+            tagCounter - Bag_counter - Clothes_counter - Shoes_counter - Accesories_counter) != 0:
+        Finded_categories.append( str( accesories ) + "," )
+    elif Accesories_counter > 1 and (
+            tagCounter - Bag_counter - Clothes_counter - Shoes_counter - Accesories_counter) == 0:
+        Finded_categories.append( str( Accesories_counter ) + " " + str( accesories ) )
+    elif Accesories_counter > 1 and (
+            tagCounter - Bag_counter - Clothes_counter - Shoes_counter - Accesories_counter) != 0:
+        Finded_categories.append( str( Accesories_counter ) + " " + str( accesories ) + "," )
+    #	if Glasses_counter == 1 and (tagCounter - Bag_counter - Clothes_counter - Shoes_counter - Accesories_counter - Glasses_counter) == 0:
+    #		Finded_categories.append(str(glasses))
+    #	elif Accesories_counter == 1 and (tagCounter - Bag_counter - Clothes_counter - Shoes_counter - Accesories_counter - Glasses_counter) != 0:
+    #		Finded_categories.append(str(glasses) + ",")
+    #	elif Accesories_counter > 1 and (tagCounter - Bag_counter - Clothes_counter - Shoes_counter - Accesories_counter - Glasses_counter) == 0:
+    #		Finded_categories.append(str(Glasses_counter) + " " + str(glasses))
+    #	elif Accesories_counter > 1 and (tagCounter - Bag_counter - Clothes_counter - Shoes_counter - Accesories_counter - Glasses_counter) != 0:
+    #		Finded_categories.append(str(Glasses_counter) + " " + str(glasses) + ",")
+    if Glasses_counter > 0:
+        Finded_categories.append( str( glasses ) + " " + str( Glasses_counter ) + " шт." )
+
+    Finded_categories_str = " ".join( Finded_categories )
+    sheet1.write( rowsInWork, 3, str( Finded_categories_str ) )
+    if tagCounter == 0:
+        sheet1.write( rowsInWork, 3, "НЕ ОБНАРУЖЕНО КЛЮЧЕЙ!" )
+        FailureAtTagAssignmentProcessCounter += 1
+        FailureAtTagAssignmentProcessList.append( (rowsInWork) )
+    counter = ((rows - 1) - rowsInWork)
+    rowsInWork += 1
+    print( "Остаётся грузов: " + str( counter ) )
+    print( "***************************************************************************************" )
+    tagCounter = 0
+    if rowsInWork == (rows):
+        print( ("Готово!").center( 60, "=" ) )
+        if FailureAtTagAssignmentProcessCounter == 0:
+            print( "Всем грузам присвоены соответствующие тэги.".center( 60 ) )
+        if FailureAtTagAssignmentProcessCounter != 0:
+            print( "\n\n\n\n\n\nВнимание: найдены ошибки!" )
+            print( "Необработанных описаний: " + str(
+                FailureAtTagAssignmentProcessCounter ) + " шт." + "Необходимо обработать вручную!" )
+            print( "Грузы без тэга: ", end="" )
+            print( *FailureAtTagAssignmentProcessList, sep=", " )
+
+nrows = sheet.nrows  # Соответствует заполнению по оси Y
+ncols = sheet.ncols  # Соответствует заполнению по оси X
 info = ""
-xlist = list(range(nrows))
-ylist = list(range(ncols))
+xlist = list( range( nrows ) )
+ylist = list( range( ncols ) )
 for x in xlist:
-	y = 0
-	info = sheet.cell_value(x, y)
-	sheet1.write(x, y, info)
+    y = 0
+    info = sheet.cell_value( x, y )
+    sheet1.write( x, y, info )
 for x in xlist:
-	y = 1
-	info = sheet.cell_value(x, y)
-	sheet1.write(x, y, info)
+    y = 1
+    info = sheet.cell_value( x, y )
+    sheet1.write( x, y, info )
 for x in xlist:
-	y = 2
-	info = sheet.cell_value(x, y)
-	sheet1.write(x, y, info)
+    y = 2
+    info = sheet.cell_value( x, y )
+    sheet1.write( x, y, info )
 
 workbookw.save(path)
-print(path)
-print("Файл обработан.")
+
+EndingScenario = input("Нажмите Enter для выхода")
